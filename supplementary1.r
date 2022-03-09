1b8463ffa5415c607906fdb834d599e8e7352554
forestgeoinformatics <94542155+forestgeoinformatics@users.noreply.github.com>
2022-03-09 16:25:49 +0530

    Update supplementary1.r

--- supplementary1.r
+++ supplementary1.r
@@ -1,5 +1,5 @@
 library(spocc)
-spp = c("Impatiens glanduliferaRoyle","Lantana camara ")
+spp = c("Impatiens glanduliferaRoyle","Lantana camara")
 dat = occ(query = spp, from ='gbif', gbifopts = list(hasCoordinate=TRUE))
 data = occ2df(dat) ##convert to data frame
 head(data)
